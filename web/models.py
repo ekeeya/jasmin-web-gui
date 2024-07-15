1f621711cdd69062a9314dbd2e10854019b7242a
Keeya Emmanuel <ekeeya@ds.co.ug>
2024-07-15 14:33:39 +0300

    -add coverage, flake

--- web/models.py
+++ web/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title
--- web/tests.py
+++ web/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Post
+
 
 # Create your tests here.
+
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        self.post = Post.objects.create(title="django", author="ekeeya", slug="django")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
